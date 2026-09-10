0dda8e0ce2a9099d1a749e360b067470f4b7c126
Maxim <m.baldikhin@georank.ru>
2026-09-10 20:25:28 +0300

    add analytics service with calculate function

--- src/app/models/bus.model.ts
+++ src/app/models/bus.model.ts
@@ -1,29 +1,17 @@
 export interface TripHistoryRecord { 
-
   date: string; 
-
   dayOfWeek: string; 
-
   scheduledTime: string; 
-
   delayMinutes: number; 
-
   routeNumber: string; 
-
 } 
 
   
 
 export interface RouteForecast { 
-
   routeNumber: string; 
-
   scheduledTime: string; 
-
   predictedTime: string; 
-
   averageDelay: number; 
-
   recordsCount: number; 
-
 } 
