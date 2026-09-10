39ba25a4a0aaa6e2a2ead65392a4eff71d454606
Maxim <m.baldikhin@georank.ru>
2026-09-10 20:21:46 +0300

    create interfaces

---
+++ src/app/models/bus.model.ts
@@ -0,0 +1,29 @@
+export interface TripHistoryRecord { 
+
+  date: string; 
+
+  dayOfWeek: string; 
+
+  scheduledTime: string; 
+
+  delayMinutes: number; 
+
+  routeNumber: string; 
+
+} 
+
+  
+
+export interface RouteForecast { 
+
+  routeNumber: string; 
+
+  scheduledTime: string; 
+
+  predictedTime: string; 
+
+  averageDelay: number; 
+
+  recordsCount: number; 
+
+} 
